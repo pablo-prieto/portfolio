1f4758bfa2d43470b6fa2d0f0cc16cf99a4b435f
Pablo Prieto Posada <pablo@Pablos-MBP.home>
2020-10-03 17:52:36 -0400

    adding to gitignore

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,27 +1,33 @@
 // Menu.js
-import React from 'react';
-import { bool } from 'prop-types';
-import { StyledMenu } from './Menu.styled';
+import React from "react"
+import { bool } from "prop-types"
+import { StyledMenu } from "./Menu.styled"
 
 const Menu = ({ open }) => {
   return (
     <StyledMenu open={open}>
       <a href="/">
-        <span role="img" aria-label="about us">&#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;</span>
+        <span role="img" aria-label="about us">
+          &#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;
+        </span>
         About
       </a>
       <a href="/">
-        <span role="img" aria-label="price">&#x1f4b8;</span>
+        <span role="img" aria-label="price">
+          &#x1f4b8;
+        </span>
         Projects
-        </a>
+      </a>
       <a href="/">
-        <span role="img" aria-label="contact">&#x1f4e9;</span>
+        <span role="img" aria-label="contact">
+          &#x1f4e9;
+        </span>
         Contact
-        </a>
+      </a>
     </StyledMenu>
   )
 }
 Menu.propTypes = {
   open: bool.isRequired,
 }
-export default Menu;
+export default Menu
